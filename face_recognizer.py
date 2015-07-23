0f2772a75e74ccf13d5950fd7ae0259d3d97399a
Filipe Ramos <Filipe@MacBook-Pro-de-Filipe.local>
2015-07-23 20:46:05 -0300

    melhoria

--- face_recognizer.py
+++ face_recognizer.py
@@ -12,7 +12,8 @@ faceCascade = cv2.CascadeClassifier(cascadePath)
 #  LBPH Face Recognizer
 recognizer = cv2.createLBPHFaceRecognizer()
 
-#Validacao do formato da imagem aceita; eh valido png,jpeg,jpg.
+
+# Validacao do formato da imagem aceita; eh valido png,jpeg,jpg.
 def is_valid_image(image_name):
     if (image_name.endswith('.jpg') or image_name.endswith('.jpeg') or image_name.endswith('.png')):
         return True
@@ -20,7 +21,6 @@ def is_valid_image(image_name):
 
 
 def get_images_and_labels(path):
-
     image_paths = [os.path.join(path, f) for f in os.listdir(path) if not 'test' in f and is_valid_image(f)]
     # conjunto com as faces extraidas
     images = []
@@ -38,11 +38,11 @@ def get_images_and_labels(path):
         nbr = int(os.path.split(image_path)[1].split(".")[1])
         # Detectar a face na Imagem.
         faces = faceCascade.detectMultiScale(
-		imagem_arrayNumPY,
-		scaleFactor=1.2,
-		minNeighbors=5,
-		minSize=(30, 30),
-		flags = cv2.cv.CV_HAAR_SCALE_IMAGE)
+            imagem_arrayNumPY,
+            scaleFactor=1.2,
+            minNeighbors=5,
+            minSize=(30, 30),
+            flags=cv2.cv.CV_HAAR_SCALE_IMAGE)
         # Se alguma face for encontrada, adicionar a face para Face e o rotulo(id) para o rotulo)
         for (x, y, w, h) in faces:
             images.append(imagem_arrayNumPY[y: y + h, x: x + w])
@@ -71,30 +71,35 @@ for image_path in image_paths:
     face_a_ser_reconhecida_tons_cinza = cv2.cvtColor(face_a_ser_reconhecida, cv2.COLOR_BGR2GRAY)
     face_a_ser_reconhecida_numPyArray = np.array(face_a_ser_reconhecida_tons_cinza, 'uint8')
     faces = faceCascade.detectMultiScale(face_a_ser_reconhecida_numPyArray,
-					scaleFactor=1.2,
-					minNeighbors=5,
-					minSize=(30, 30),
-					flags = cv2.cv.CV_HAAR_SCALE_IMAGE)
+                                         scaleFactor=1.2,
+                                         minNeighbors=5,
+                                         minSize=(30, 30),
+                                         flags=cv2.cv.CV_HAAR_SCALE_IMAGE)
     for (x, y, w, h) in faces:
-        nbr_predicted, conf = recognizer.predict(face_a_ser_reconhecida_numPyArray[y: y + h, x: x + w])
+        nbr_predicted2, conf = recognizer.predict(face_a_ser_reconhecida_numPyArray[y: y + h, x: x + w])
         nbr_actual = int(os.path.split(image_path)[1].split(".")[1])
-        threshold = 100
-        if nbr_actual == nbr_predicted and conf <= 90 :
+        # threshold = 100
+        if nbr_actual == nbr_predicted2 and conf <= 90:
             true_confidence = 100 - conf;
             print "{} eh corretamente reconhecido com nivel de confianca {}".format(nbr_actual, true_confidence)
             cv2.rectangle(face_a_ser_reconhecida, (x, y), (x + w, y + h), (0, 255, 0), 2)
             faces_corretamente_reconhecidas += 1
             cv2.imshow("Face reconhecida", face_a_ser_reconhecida)
-        elif nbr_actual != nbr_predicted and conf <= 10:
-            print "{} eh incorretamente reconhecido como {} com nivel de confianca {}".format(nbr_actual, nbr_predicted, 100 - conf)
-	    print conf
+        elif nbr_actual != nbr_predicted2 and conf <= 10:
+            print "{} eh incorretamente reconhecido como {} com nivel de confianca {}".format(nbr_actual,
+                                                                                              nbr_predicted2,
+                                                                                              100 - conf)
+            print conf
             faces_Incorretamente_reconhecidas += 1
-	    cv2.rectangle(face_a_ser_reconhecida, (x, y), (x + w, y + h), (0, 255, 0), 2)
-	    cv2.imshow("Face incorretamente reconhecida", face_a_ser_reconhecida)
+            cv2.rectangle(face_a_ser_reconhecida, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            cv2.imshow("Face incorretamente reconhecida", face_a_ser_reconhecida)
         cv2.waitKey(1000)
-print "acertos: {}\nerros:(falso positivo) {}".format(faces_corretamente_reconhecidas, faces_Incorretamente_reconhecidas)
-print "rostos nao reconhecidos e que eram pra ser:(falso negativo) {}".format(int(rostos_previamente_verificados)-faces_corretamente_reconhecidas)
-print "precisao: {}".format(faces_corretamente_reconhecidas / (faces_corretamente_reconhecidas+faces_Incorretamente_reconhecidas))
+print "acertos: {}\nerros:(falso positivo) {}".format(faces_corretamente_reconhecidas,
+                                                      faces_Incorretamente_reconhecidas)
+print "rostos nao reconhecidos e que eram pra ser:(falso negativo) {}".format(
+    int(rostos_previamente_verificados) - faces_corretamente_reconhecidas)
+print "precisao: {}".format(
+    faces_corretamente_reconhecidas / (faces_corretamente_reconhecidas + faces_Incorretamente_reconhecidas))
 log_path = os.path.join(training_set, training_set + "_log.txt")
 log = open(log_path, "w")
 log.write("acertos\n" + str(faces_corretamente_reconhecidas))
