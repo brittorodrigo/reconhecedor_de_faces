529b69eccd71c9f6fbc2f3e0a8faa2dd5f018814
brittorodrigo <rodrigo.debritto@gmail.com>
2015-07-23 19:58:03 -0300

    mudanças nos parametros da funcao de deteccao e no limiar

--- face_recognizer.py
+++ face_recognizer.py
@@ -37,7 +37,12 @@ def get_images_and_labels(path):
         # Obtendo o rotulo da imagem, ID
         nbr = int(os.path.split(image_path)[1].split(".")[1])
         # Detectar a face na Imagem.
-        faces = faceCascade.detectMultiScale(imagem_arrayNumPY)
+        faces = faceCascade.detectMultiScale(
+		imagem_arrayNumPY,
+		scaleFactor=1.2,
+		minNeighbors=5,
+		minSize=(30, 30),
+		flags = cv2.cv.CV_HAAR_SCALE_IMAGE)
         # Se alguma face for encontrada, adicionar a face para Face e o rotulo(id) para o rotulo)
         for (x, y, w, h) in faces:
             images.append(imagem_arrayNumPY[y: y + h, x: x + w])
@@ -57,32 +62,39 @@ cv2.destroyAllWindows()
 # Realizar o treinamento baseado no conjunto de imagens fornecidas.
 recognizer.train(images, np.array(labels))
 
-path = './bdtestkjell'
+path = './bdtest'
 image_paths = [os.path.join(path, f) for f in os.listdir(path)]
 faces_corretamente_reconhecidas = 0
-faces_INcorretamente_reconhecidas = 0
+faces_Incorretamente_reconhecidas = 0
 for image_path in image_paths:
     face_a_ser_reconhecida = cv2.imread(image_path)
     face_a_ser_reconhecida_tons_cinza = cv2.cvtColor(face_a_ser_reconhecida, cv2.COLOR_BGR2GRAY)
     face_a_ser_reconhecida_numPyArray = np.array(face_a_ser_reconhecida_tons_cinza, 'uint8')
-    faces = faceCascade.detectMultiScale(face_a_ser_reconhecida_numPyArray)
+    faces = faceCascade.detectMultiScale(face_a_ser_reconhecida_numPyArray,
+					scaleFactor=1.2,
+					minNeighbors=5,
+					minSize=(30, 30),
+					flags = cv2.cv.CV_HAAR_SCALE_IMAGE)
     for (x, y, w, h) in faces:
         nbr_predicted, conf = recognizer.predict(face_a_ser_reconhecida_numPyArray[y: y + h, x: x + w])
         nbr_actual = int(os.path.split(image_path)[1].split(".")[1])
         threshold = 100
-        if nbr_actual == nbr_predicted and conf < 100:
+        if nbr_actual == nbr_predicted and conf <= 90 :
             true_confidence = 100 - conf;
             print "{} eh corretamente reconhecido com nivel de confianca {}".format(nbr_actual, true_confidence)
             cv2.rectangle(face_a_ser_reconhecida, (x, y), (x + w, y + h), (0, 255, 0), 2)
             faces_corretamente_reconhecidas += 1
             cv2.imshow("Face reconhecida", face_a_ser_reconhecida)
-        elif nbr_actual != nbr_predicted and conf < 100:
-            print "{} eh incorretamente reconhecido como {}".format(nbr_actual, nbr_predicted)
-            faces_INcorretamente_reconhecidas += 1
+        elif nbr_actual != nbr_predicted and conf <= 10:
+            print "{} eh incorretamente reconhecido como {} com nivel de confianca {}".format(nbr_actual, nbr_predicted, 100 - conf)
+	    print conf
+            faces_Incorretamente_reconhecidas += 1
+	    cv2.rectangle(face_a_ser_reconhecida, (x, y), (x + w, y + h), (0, 255, 0), 2)
+	    cv2.imshow("Face incorretamente reconhecida", face_a_ser_reconhecida)
         cv2.waitKey(1000)
-print "acertos: {}\nerros: {}".format(faces_corretamente_reconhecidas, faces_INcorretamente_reconhecidas)
+print "acertos: {}\nerros: {}".format(faces_corretamente_reconhecidas, faces_Incorretamente_reconhecidas)
 log_path = os.path.join(training_set, training_set + "_log.txt")
 log = open(log_path, "w")
 log.write("acertos\n" + str(faces_corretamente_reconhecidas))
-log.write("\nerros\n" + str(faces_INcorretamente_reconhecidas))
+log.write("\nerros\n" + str(faces_Incorretamente_reconhecidas))
 log.close()
