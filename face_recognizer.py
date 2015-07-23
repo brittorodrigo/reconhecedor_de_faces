a73e68b0260b945de821655b8211f22d25f3cca2
Filipe Ramos <Filipe@MacBook-Pro-de-Filipe.local>
2015-07-23 18:17:06 -0300

    melhorias

--- face_recognizer.py
+++ face_recognizer.py
@@ -1,90 +1,88 @@
 #!/usr/bin/python
 
-# Import the required modules
 import cv2, os
 import numpy as np
 import sys
 
-# For face detection we will use the Haar Cascade provided by OpenCV.
+# Utilizando Haar Cascade do OpenCV para detectar as faces.
 cascadePath = "haarcascade_frontalface_default.xml"
 faceCascade = cv2.CascadeClassifier(cascadePath)
 
-# For face recognition we will the the LBPH Face Recognizer
+# Utilizaremos para criar o modelo da face que queremos reconhecer o
+#  LBPH Face Recognizer
 recognizer = cv2.createLBPHFaceRecognizer()
 
-
+#Validacao do formato da imagem aceita; eh valido png,jpeg,jpg.
 def is_valid_image(image_name):
-    if(image_name.endswith('.jpg') or image_name.endswith('.jpeg')  or image_name.endswith('.png')):
-	 return True
+    if (image_name.endswith('.jpg') or image_name.endswith('.jpeg') or image_name.endswith('.png')):
+        return True
     return False
 
+
 def get_images_and_labels(path):
-    # Append all the absolute image paths in a list image_paths
-    # We will not read the image with the .sad extension in the training set
-    # Rather, we will use them to test our accuracy of the training
+
     image_paths = [os.path.join(path, f) for f in os.listdir(path) if not 'test' in f and is_valid_image(f)]
-    # images will contains face images
+    # conjunto com as faces extraidas
     images = []
-    # labels will contains the label that is assigned to the image
+    # rotulo da imagem
     labels = []
     for image_path in image_paths:
-	print image_path
-        # Read the image and convert to grayscale
-	input_image = cv2.imread(image_path)
-        image_pil = cv2.cvtColor(input_image, cv2.COLOR_BGR2GRAY)
-        # Convert the image format into numpy array
-        image = np.array(image_pil, 'uint8')
-        # Get the label of the image
-	nbr = int(os.path.split(image_path)[1].split(".")[1])
-        # Detect the face in the image
-        faces = faceCascade.detectMultiScale(image)
-        # If face is detected, append the face to images and the label to labels
+        print image_path
+        # Ler a imagem.
+        imagem_de_entrada = cv2.imread(image_path)
+        # Converter a imagem para tons de cinza
+        imagem_tons_de_cinza = cv2.cvtColor(imagem_de_entrada, cv2.COLOR_BGR2GRAY)
+        # Criando um Numpy Array
+        imagem_arrayNumPY = np.array(imagem_tons_de_cinza, 'uint8')
+        # Obtendo o rotulo da imagem, ID
+        nbr = int(os.path.split(image_path)[1].split(".")[1])
+        # Detectar a face na Imagem.
+        faces = faceCascade.detectMultiScale(imagem_arrayNumPY)
+        # Se alguma face for encontrada, adicionar a face para Face e o rotulo(id) para o rotulo)
         for (x, y, w, h) in faces:
-            images.append(image[y: y + h, x: x + w])
+            images.append(imagem_arrayNumPY[y: y + h, x: x + w])
             labels.append(nbr)
-            #cv2.imshow("Adding faces to traning set...", image[y: y + h, x: x + w])
+            # cv2.imshow("Adding faces to traning set...", image[y: y + h, x: x + w])
             cv2.waitKey(50)
-    # return the images list and labels list
     return images, labels
 
-# Path to the training dataset
-training_set= sys.argv[1]
-# Call the get_images_and_labels function and get the face images and the
-# corresponding labels
+
+# Caminho do computador para o conjunto de treinamento
+training_set = sys.argv[1]
+
+
 images, labels = get_images_and_labels(training_set)
 cv2.destroyAllWindows()
 
-# Perform the tranining
+# Realizar o treinamento baseado no conjunto de imagens fornecidas.
 recognizer.train(images, np.array(labels))
 
-# Append the images with the extension .sad into image_paths
-path = './bdtest'			
+path = './bdtestkjell'
 image_paths = [os.path.join(path, f) for f in os.listdir(path)]
-correct_matches = 0
-incorrect_matches = 0
+faces_corretamente_reconhecidas = 0
+faces_INcorretamente_reconhecidas = 0
 for image_path in image_paths:
-    input_predict_image = cv2.imread(image_path)
-    predict_image_pil = cv2.cvtColor(input_predict_image, cv2.COLOR_BGR2GRAY)
-    predict_image = np.array(predict_image_pil, 'uint8')
-    faces = faceCascade.detectMultiScale(predict_image)
+    face_a_ser_reconhecida = cv2.imread(image_path)
+    face_a_ser_reconhecida_tons_cinza = cv2.cvtColor(face_a_ser_reconhecida, cv2.COLOR_BGR2GRAY)
+    face_a_ser_reconhecida_numPyArray = np.array(face_a_ser_reconhecida_tons_cinza, 'uint8')
+    faces = faceCascade.detectMultiScale(face_a_ser_reconhecida_numPyArray)
     for (x, y, w, h) in faces:
-        nbr_predicted, conf = recognizer.predict(predict_image[y: y + h, x: x + w])
+        nbr_predicted, conf = recognizer.predict(face_a_ser_reconhecida_numPyArray[y: y + h, x: x + w])
         nbr_actual = int(os.path.split(image_path)[1].split(".")[1])
-	threshold = 100
-        if nbr_actual == nbr_predicted  and conf > 2 and conf < 100:
-	    true_confidence = 100 - conf;
+        threshold = 100
+        if nbr_actual == nbr_predicted and conf < 100:
+            true_confidence = 100 - conf;
             print "{} eh corretamente reconhecido com nivel de confianca {}".format(nbr_actual, true_confidence)
-	    cv2.rectangle(input_predict_image, (x, y), (x+w, y+h), (0, 255, 0), 2)
-	    correct_matches += 1
-	    cv2.imshow("Face reconhecida" ,input_predict_image)
-        elif nbr_actual != nbr_predicted  and  conf < 100:
+            cv2.rectangle(face_a_ser_reconhecida, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            faces_corretamente_reconhecidas += 1
+            cv2.imshow("Face reconhecida", face_a_ser_reconhecida)
+        elif nbr_actual != nbr_predicted and conf < 100:
             print "{} eh incorretamente reconhecido como {}".format(nbr_actual, nbr_predicted)
-	    incorrect_matches += 1
+            faces_INcorretamente_reconhecidas += 1
         cv2.waitKey(1000)
-print "acertos: {}\nerros: {}".format(correct_matches, incorrect_matches)
+print "acertos: {}\nerros: {}".format(faces_corretamente_reconhecidas, faces_INcorretamente_reconhecidas)
 log_path = os.path.join(training_set, training_set + "_log.txt")
 log = open(log_path, "w")
-log.write("acertos\n" + str(correct_matches))
-log.write("\nerros\n" + str(incorrect_matches) )
+log.write("acertos\n" + str(faces_corretamente_reconhecidas))
+log.write("\nerros\n" + str(faces_INcorretamente_reconhecidas))
 log.close()
-
